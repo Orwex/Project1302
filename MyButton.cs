4012ad31c6b39cbda18065ec506ba74befa1b185
Orwex <44752155+Orwex@users.noreply.github.com>
2019-02-13 21:35:30 +0200

    Add files via upload

---
+++ MyButton.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1302
+{
+    class MyButton
+    {
+        protected Point topLeft;
+        protected Point bottomRight;
+
+        private int width, height;
+
+        internal MyButton(Point topLeft,Point bottomRight)
+        {
+            this.topLeft = topLeft;
+            this.bottomRight = bottomRight;
+        }
+
+        internal int GetWidth()
+        {
+            return this.width;
+        }
+
+        internal int GetHeight()
+        {
+            return this.height;
+
+        }
+
+        internal bool SetTopLeft (Point p)
+        {
+            if ((p.GetX() < this.bottomRight.GetX()) && (p.GetY() > this.bottomRight.GetY())) 
+            {
+                this.topLeft = p;
+                this.height = 5;
+                this.width = 5;
+                return true;
+            }
+
+            else 
+            {
+                Console.WriteLine("Point is not on the top left of Button");
+                return false;
+            }
+        }
+
+        internal bool SetBottomRight (Point p)
+        {
+            if ((p.GetX() > this.topLeft.GetX()) && (p.GetY() < this.topLeft.GetY()))
+            {
+                this.bottomRight = p;
+                this.height = 5;
+                this.width = 5;
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine("Point is not on the bottom right of Button");
+                return false;
+            }
+        }
+
+        internal Point GetTopLeft()
+        {
+            return topLeft;
+        }
+
+        internal Point GetBottomRight()
+        {
+            return bottomRight;
+        }
+
+        public override string ToString()
+        {
+            return $"Width {this.width}, Height {this.height}, Top Point {this.topLeft}, Bottom Right {this.bottomRight}";
+        }
+
+    }
+}
---
+++ MyCanvas.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1302
+{
+    class MyCanvas
+    {
+        public const int MaxWidth = 800;
+        public const int MaxHeight = 600;
+        private static int buttonIndex = 0;
+        private static int MaxButtons = 3;
+        private static MyButton[] buttons = new MyButton[MaxButtons];
+
+
+        public static bool CreateNewButton(int x1, int y1, int x2, int y2)
+        {
+            bool toContinue = true;
+            buttonIndex = 0;
+
+            if (buttonIndex < MaxButtons)
+            {
+                Point NewPoint_TopLeft = new Point(x1, y1);
+                Point NewPoint_BottomRight = new Point(x2, y2);
+                buttons[buttonIndex].SetTopLeft(NewPoint_TopLeft);
+                buttons[buttonIndex].SetBottomRight(NewPoint_BottomRight);
+                buttonIndex++;
+                toContinue = true;
+            }
+
+            else
+            {
+                toContinue = false;
+                return toContinue;
+            }
+        }
+
+        public static bool MoveButton(int buttonNumber, int x, int y)
+        {
+            int oldX, oldY;
+            oldX = buttons[buttonNumber].GetBottomRight().GetX();
+            oldY = buttons[buttonNumber].GetBottomRight().GetY();
+            buttons[buttonNumber].SetBottomRight(new Point(oldX + x, oldY + y));
+            oldX = buttons[buttonNumber].GetTopLeft().GetX();
+            oldY = buttons[buttonNumber].GetTopLeft().GetY();
+            buttons[buttonNumber].SetTopLeft(new Point(oldX + x, oldY + y));
+
+            return true;
+
+        }
+
+        public static bool DeleteLastButton()
+        {
+            if (buttonIndex > 0)
+            {
+                buttons[buttonIndex].SetTopLeft(new Point (0, 0));
+                buttons[buttonIndex].SetBottomRight(new Point(0, 0));
+                buttonIndex--;
+                return true
+            }
+
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static void ClearAllButtons()
+        {
+            if (buttonIndex > 0)
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    buttons[i].SetTopLeft(new Point(0, 0));
+                    buttons[i].SetBottomRight(new Point(0, 0));
+                }
+
+                buttonIndex = 0;
+            }
+        }
+
+        private static int GetCurrentNumberOfButtons()
+        {
+            return (buttonIndex + 1);
+        }
+
+        public static int GetMaxNumberOfButtons()
+        {
+            return MaxButtons;
+        }
+
+        public static int GetTheMaxWidthOfAButton()
+        {
+            int MaxWidthOfAButton = 0;
+            if (buttonIndex > 0)
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    if ((buttons[i].GetBottomRight().GetX() - buttons[i].GetTopLeft().GetX()) > MaxWidthOfAButton);
+                    MaxWidthOfAButton = buttons[i].GetBottomRight().GetX() - buttons[i].GetTopLeft().GetX();                   
+                }
+                return MaxWidthOfAButton;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static int GetTheMaxHeightOfAButton()
+        {
+            int MaxHeightOfAButton = 0;
+            if (buttonIndex > 0)
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    if ((buttons[i].GetTopLeft().GetY() - buttons[i].GetBottomRight().GetY()) > MaxHeightOfAButton);
+                    MaxHeightOfAButton = buttons[i].GetBottomRight().GetX() - buttons[i].GetTopLeft().GetX();
+                }
+               
+            }
+            return MaxHeightOfAButton;
+        }
+    }
+}
